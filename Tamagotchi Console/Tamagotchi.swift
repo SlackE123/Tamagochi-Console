2940384f4dc49ebc47e4f35fff9af16bd122a709
Ed Slack <Slack.e@etoncollege.org.uk>
2023-11-21 11:08:47 +0000

    Initial Commit of tamagotchi

---
+++ Tamagotchi Console/Tamagotchi.swift
@@ -0,0 +1,50 @@
+//
+//  Tamagochi.swift
+//  Tamagochi Console
+//
+//  Created by Slack, Ed (AMM) on 21/11/2023.
+//
+
+import Foundation
+
+class Tamagotchi {
+    let name: String
+    
+    var weight: Int
+    var hunger: Int
+    var happiness: Int
+    var discipline: Int
+    var sick: Bool
+    var alive: Bool
+    var awake: Bool
+    
+    init(name: String) {
+        weight = 5
+        hunger = 0
+        happiness = 0
+        discipline = 0
+        sick = false
+        alive = true
+        awake = true
+        self.name = name
+        
+        
+    }
+    
+    func eatSnack() {
+        hunger -= 1
+        weight += 2
+    }
+    
+    func eatMeal() {
+        hunger = 4
+        weight += 1
+    }
+    
+    func playGame() {
+        weight -= 1
+        happiness += 1
+    }
+    
+    
+}
---
+++ Tamagotchi Console/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Tamagochi Console
+//
+//  Created by Slack, Ed (AMM) on 21/11/2023.
+//
+
+import Foundation
+
+let lillyTamagotchi = Tamagotchi(name: "lilly")
+
+print(lillyTamagotchi.happiness)
+
+lillyTamagotchi.eatSnack()
+print(lillyTamagotchi.weight)
